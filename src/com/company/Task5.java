3f1a2a55945dcd4b4218e9763ee636d0c36665ca
bhosa <bhosalekaran08@gmail.com>
2026-02-11 19:13:47 +0530

    printing web scraping

--- src/com/company/Task5.java
+++ src/com/company/Task5.java
@@ -6,5 +6,6 @@ public class Task5 {
     public static void main(String[] args) {
 
         System.out.println("----- WEB SCRAPING -----");
+        System.out.println("Under Development");
     }
 }
